ed54d12ade8cb260a28e44297ea436a8b19d93b9
Carla-Daniela <daninevesdn22@gmail.com>
2022-10-19 20:40:08 -0300

    primeiro codigo em CSharp usando um exemplo de calculadora

---
+++ Calculadora.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace testec_
+{
+    class Calculadora
+    {
+        
+      public void Soma(int numero1, int numero2){
+        System.Console.WriteLine(numero1+numero2);
+      }
+      public void Subtracao(int numero1, int numero2){
+        System.Console.WriteLine(numero1-numero2);
+      }
+      public void Multiplicacao(int numero1, int numero2){
+        System.Console.WriteLine(numero1*numero2);
+      }
+      public void Divisao(int numero1, int numero2){
+        System.Console.WriteLine(numero1/numero2);
+      }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace testec_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculadora calc=new Calculadora();
+            int opcao,num1,num2;
+
+           System.Console.WriteLine(" Calculadora:\nDigite uma das opções abaixo:");
+           System.Console.WriteLine(" 1-Soma\n 2-Subtração\n 3-Multiplicação\n 4-Divisão");
+           
+           opcao=Convert.ToInt32(Console.ReadLine());
+           System.Console.WriteLine("Digite os valores:");
+           num1=Convert.ToInt32(Console.ReadLine());
+           num2=Convert.ToInt32(Console.ReadLine());
+
+           if(opcao==1){
+                calc.Soma(num1,num2);
+           }
+           else if(opcao==2){
+                calc.Subtracao(num1,num2);
+           }
+           else if(opcao==3){
+                calc.Multiplicacao(num1,num2);
+           }
+           else if(opcao==4){
+                if(num2!=0){
+                calc.Divisao(num1,num2);
+                }
+                else{
+                    System.Console.WriteLine("valo invalido, não existe divisão por 0");
+                    num2=Convert.ToInt32(Console.ReadLine());
+                    calc.Divisao(num1,num2);
+                }
+           }
+           else{
+             System.Console.WriteLine("opção não existe!");
+           }
+            
+        }
+    }
+}
